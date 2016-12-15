05426d3d73f2b5f6dabaf4dd5846bb08eeb719c9
Caleb Mills <xeno.cmills@gmail.com>
2016-12-14 17:35:46 -0700

    Made progress

--- ReduxSimpleStarter/src/index.js
+++ ReduxSimpleStarter/src/index.js
@@ -1,17 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyCwVr_w7x4Fqy8BhmW9qfMOM0WMb7xpNoo';
 
-// Create a new component. This component should produce some HTML
-const App = () => {
-    return (
-        <div>
-            <SearchBar / >
-        </div>
-    );
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'after the burial' }, (videos) => {
+            this.setState({ videos });
+        });
+    }
+
+    render() {
+        return ( 
+            <div>
+                <SearchBar />
+                <VideoDetail video={this.state.videos[0]} />
+                <VideoList videos={this.state.videos} />
+            </div>
+        );
+    }
 }
 
-ReactDOM.render(<App />, document.querySelector('.container'));
+ReactDOM.render( <App /> , document.querySelector('.container'));
